57e6a983e5327e52642b4275b85ba1c3949cdc43
Chirag Chandrashekhar <cchirag85@gmail.com>
2024-09-26 11:47:35 +0530

    updated the copy dir code

--- scripts/build.go
+++ scripts/build.go
@@ -1,23 +1,86 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
-	"os/exec"
+	"path"
 	"path/filepath"
 )
 
+func copyFile(src, dst string) error {
+	var err error
+	var srcfd *os.File
+	var dstfd *os.File
+	var srcinfo os.FileInfo
+
+	if srcfd, err = os.Open(src); err != nil {
+		return err
+	}
+	defer srcfd.Close()
+
+	if dstfd, err = os.Create(dst); err != nil {
+		return err
+	}
+	defer dstfd.Close()
+
+	if _, err = io.Copy(dstfd, srcfd); err != nil {
+		return err
+	}
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+	}
+	return os.Chmod(dst, srcinfo.Mode())
+}
+
+func copyDir(src string, dst string) error {
+	var err error
+	var fds []os.DirEntry
+	var srcinfo os.FileInfo
+
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(dst, srcinfo.Mode()); err != nil {
+		return err
+	}
+
+	if fds, err = os.ReadDir(src); err != nil {
+		return err
+	}
+	for _, fd := range fds {
+		srcfp := path.Join(src, fd.Name())
+		dstfp := path.Join(dst, fd.Name())
+
+		if fd.IsDir() {
+			if err = copyDir(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			if err = copyFile(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+	return nil
+}
 func main() {
 	cwd, err := os.Getwd()
 
 	if err != nil {
 		log.Panic(err)
 	}
-	dest := filepath.Join(cwd, "_site/")
+
+	dest := filepath.Join(cwd, "_site")
 
 	source := filepath.Join(cwd, "public")
+	os.MkdirAll(dest, os.ModePerm)
 
-	cmd := exec.Command("cp", "--recursive", source, dest)
-	cmd.Run()
+	copyErr := copyDir(source, dest)
 
+	if copyErr != nil {
+		log.Panic(copyErr)
+	}
 }
